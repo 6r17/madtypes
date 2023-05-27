904a24ae2f723f515ddc3950647392651682f105
Patrick Borowy <112330025+6r17@users.noreply.github.com>
2023-05-27 02:16:52 +0200

    Update setup.py for a more appropriate tone

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version="0.0.1",
     author="6r17",
     author_email="patrick.borowy@proton.me",
-    description="Python typing that will actually tell you to *** at runtime",
+    description="Python typing that will raise TypeError at runtime",
     long_description=long_description,
     packages=find_packages(include=["madtypes"]),
     install_requires=[],
