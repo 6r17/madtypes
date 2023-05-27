1c9ba9181f75bb67f5690d04c74bec5204f4d7c2
Patrick Borowy <112330025+6r17@users.noreply.github.com>
2023-05-27 02:11:39 +0200

    Update setup.py to include long_description and keywords

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="madtypes",
     version="0.0.1",
     author="6r17",
     author_email="patrick.borowy@proton.me",
-    description="Python typing that will actually tell you to <censored>",
+    description="Python typing that will actually tell you to *** at runtime",
+    long_description=long_description,
     packages=find_packages(include=["madtypes"]),
     install_requires=[],
     extras_require={"dev": ["pytest", "pytest-cov"]},
+    keywords='typing',
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
